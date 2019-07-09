754bda7555ce34651c08ca4e0664e81abcbc74b1
AnnoyedSloth <sksk31052@hanmail.net>
2019-07-09 18:52:24 +0900

    Implementing Animation related things

---
+++ Source/ArenaBattle/Public/ABAnimInstance.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ABAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENABATTLE_API UABAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
